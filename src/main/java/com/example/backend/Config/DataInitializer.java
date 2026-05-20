89ded8970633fff29ecf71bc00739e185f0a0ad8
Marine CEDE <marine.cede2025@campus-eni.fr>
2026-05-20 10:05:59 +0200

    feat: ajout de jeu de donnees

--- src/main/java/com/example/backend/Config/DataInitializer.java
+++ src/main/java/com/example/backend/Config/DataInitializer.java
@@ -59,6 +59,26 @@ public class DataInitializer implements CommandLineRunner {
         Livre l4 = livreRepo.save(new Livre(null, "Clean Code", "Robert Martin", "Tech", "Apprendre à coder propre", "ISBN004", 1, LocalDate.now(), null));
         Livre l5 = livreRepo.save(new Livre(null, "2024", "George Orwell", "Conte", "Dessine-moi un futur", "ISBN005", 2, LocalDate.now(), null));
         Livre l6 = livreRepo.save(new Livre(null, "La Servante écarlate", "Margaret Atwood", "Dystopie", "Dans une société du futur, la République de Gilead, les femmes ont perdu tous leurs droits et sont entièrement soumises aux hommes....", "ISBN006", 2, LocalDate.now(), null));
+        Livre l7 = livreRepo.save(new Livre(null, "Fourth wings", "Rebecca Yarros", "Fantasy", "Dragoooons graouuuuuuuu", "ISBN007", 10, LocalDate.now(), null));
+        Livre l8 = livreRepo.save(new Livre(null, "ACOTAR", "Sarah J Maas", "Fantasy", "Des Faes trop bg et des intrigues trop intrigantes", "ISBN008", 5, LocalDate.now(), null));
+        Livre l9 = livreRepo.save(new Livre(null, "Le trône de verre", "Sarah J Maas", "Fantasy", "Une histoire prenante mais bien longue quand même", "ISBN009", 3, LocalDate.now(), null));
+        Livre l10 = livreRepo.save(new Livre(null, "Crescent City", "Sarah J Maas", "Fantasy", "Y a un ange bg, et une meuf bg. Ah de la vengeance aussi", "ISBN0010", 8, LocalDate.now(), null));
+        Livre l11 = livreRepo.save(new Livre(null, "Assistant to the villain", "Hannah Nicole Maehrer", "Fantasy", "Un villain qu'est quand même vâchement attachant", "ISBN0011", 22, LocalDate.now(), null));
+        Livre l12 = livreRepo.save(new Livre(null, "Thiziri", "Alexiane de Lys", "Fantasy", "Une albinos, un alien, bref, Romaeo et Juliette 2.0", "ISBN0012", 4, LocalDate.now(), null));
+        Livre l13 = livreRepo.save(new Livre(null, "Le secret de Lomé", "Alexiane de Lys", "Fantasy", "Une biatch du lycéen qui se retrouve dans un monde où elle est devenue une esclave", "ISBN0013", 12, LocalDate.now(), null));
+        Livre l14 = livreRepo.save(new Livre(null, "La sphère", "Alexiane de Lys", "Science Fiction", "La Vr des fois c'est chaud même", "ISBN0014", 42, LocalDate.now(), null));
+        Livre l15 = livreRepo.save(new Livre(null, "De sang, d'écume et de glace", "Alexiane de Lys", "Fantasy", "Sous l'océan, sous l'océan ! Bah c'est pas si ouf hein", "ISBN0015", 0, LocalDate.now(), null)); // Stock vide
+        Livre l16 = livreRepo.save(new Livre(null, "Les fourmis", "Bernard Werber", "Science Fiction", "Les oiseaux de Hitchckok mais avec des fourmis", "ISBN0016", 5, LocalDate.now(), null));
+        Livre l17 = livreRepo.save(new Livre(null, "L'appel de Cthulhu", "H.P. Lovecraft", "Horreur", "Quand tes cauchemards font de toi quelqu'un de célèbre", "ISBN0017", 666, LocalDate.now(), null));
+        Livre l18 = livreRepo.save(new Livre(null, "Le cauchemard d'Innsmouth", "H.P. Lovecraft", "Horreur", "T'as carrément influencé el famosso King quoi !", "ISBN0018", 666, LocalDate.now(), null));
+        Livre l19 = livreRepo.save(new Livre(null, "Shining", "Stephan King", "Thriller", "Jamais vu un couloir aussi flippant", "ISBN0019", 2, LocalDate.now(), null));
+        Livre l20 = livreRepo.save(new Livre(null, "Simetierre", "Stephan King", "Horreur", "Y a un chat sur la couverture", "ISBN0020", 12, LocalDate.now(), null));
+        Livre l21 = livreRepo.save(new Livre(null, "Leontus seigneur solaire", "Rob Young", "Fantasy", "POUR L'EMPEREUR", "ISBN0021", 20, LocalDate.now(), null));
+        Livre l22 = livreRepo.save(new Livre(null, "Les chiens de Baskerville", "Arthur Conan Doyle", "Policier", "C'est élémentaire, voyons", "ISBN0022", 22, LocalDate.now(), null));
+        Livre l23 = livreRepo.save(new Livre(null, "La lignés des maudits", "Bleuenn Guillou", "Policier", "Vous connaissez la série Medium ? Bah c'est un peu la même", "ISBN0023", 23, LocalDate.now(), null));
+        Livre l24 = livreRepo.save(new Livre(null, "Cherub", "Robert Muchamore", "Espionnage", "Kings Man existait pas encore que ce gars a fait un Banger inconnu", "ISBN0024", 5, LocalDate.now(), null));
+        Livre l25 = livreRepo.save(new Livre(null, "Affronter la nuit", "Marie Reppelin", "Fantasy", "C'est Fourth wings mais en plus compréhensif", "ISBN0025", 6, LocalDate.now(), null));
+
 
         // 5. CRÉATION DES EMPRUNTS (Test des dates et retards)
         // Emprunt terminé (rendu à l'heure)
